7dd4dfedf673b9f66774af0fe47878e66616ee0f
David Susanto <davidsusanto.id@gmail.com>
2026-02-13 22:28:37 +0700

    add category

--- src/main/java/com/expense/models/Transaction.java
+++ src/main/java/com/expense/models/Transaction.java
@@ -7,15 +7,17 @@ public class Transaction {
     private LocalDate date;
     private String description;
     private double amount;
+    private String category;
 
     public Transaction() {
         
     }
     
-    public Transaction(LocalDate date, String description, double amount) {
+    public Transaction(LocalDate date, String description, double amount, String category) {
         this.date = date;
         this.description = description;
         this.amount = amount;
+        this.category = category;
     }
 
     // Getter & Setter
@@ -41,5 +43,13 @@ public class Transaction {
 
     public void setAmount(double amount) {
         this.amount = amount;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
     }
 }
